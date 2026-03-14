56715e1505568931d8afca4bf4d8c209629db10e
cookiee0409 <pysung0409@gmail.com>
2026-03-15 01:32:08 +0900

    Add files via upload

--- netlify/functions/ticker.js
+++ netlify/functions/ticker.js
@@ -1,11 +1,23 @@
 
-const ids = ["bitcoin","ethereum","solana","binancecoin","avalanche-2","sui","polkadot","matic-network"].join(",");
-export async function handler() {
-  try {
-    const res = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=usd&include_24hr_change=true`, { headers: { "accept":"application/json" } });
-    const data = await res.json();
-    return { statusCode: 200, headers: { "content-type":"application/json; charset=utf-8" }, body: JSON.stringify(data) };
-  } catch (err) {
-    return { statusCode: 200, headers: { "content-type":"application/json; charset=utf-8" }, body: JSON.stringify({}) };
-  }
+export async function handler(){
+
+try{
+
+const r=await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana&vs_currencies=usd")
+const j=await r.json()
+
+return{
+statusCode:200,
+body:JSON.stringify(j)
+}
+
+}catch{
+
+return{
+statusCode:200,
+body:"{}"
+}
+
+}
+
 }
